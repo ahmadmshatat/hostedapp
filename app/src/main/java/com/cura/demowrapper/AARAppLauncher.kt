6cc7c6daefe04d0c875f820172e8d7d98d70114c
Ahmed Shatat <ashatat@cura.healthcare>
2023-07-06 11:12:30 +0300

    init

---
+++ app/src/main/java/com/cura/demowrapper/AARAppLauncher.kt
@@ -0,0 +1,18 @@
+package com.cura.demowrapper
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+//import com.cura.demolibrary.MainActivity
+//import io.ionic.starter.MainActivity
+class AARAppLauncher : Activity() {
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        // Launch the MainActivity
+//        val intent = Intent(this, MainActivity::class.java)
+//        startActivity(intent)
+//        finish()
+//    }
+}
